4eb0347afdcbf2567360ff31081bf8a71d1f8d68
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-09-10 00:38:48 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import "./App.css";
 import Loader from "./components/Loader/Loader";
 import Navigation from "./components/Navigation/Navigation";
 import { Route, Routes } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { apiRefresh } from "./redux/auth/operations";
+import { selectAuthIsRefreshing } from "./redux/auth/selectors";
 const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
 const ContactsPage = lazy(() => import("./pages/ContactsPage/ContactsPage"));
 const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage"));
@@ -14,12 +15,15 @@ const RegistrationPage = lazy(() =>
 
 function App() {
   const dispatch = useDispatch();
+  const isRefreshing = useSelector(selectAuthIsRefreshing);
 
   useEffect(() => {
     dispatch(apiRefresh);
   }, [dispatch]);
 
-  return (
+  return isRefreshing ? (
+    <Loader />
+  ) : (
     <>
       <Navigation />
       <main>
