9d8ccc670dcab3432088eabe24ddf10a35bcc73f
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-09-13 22:06:58 +0200

    create text home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,12 +3,11 @@ import css from "./HomePage.module.css";
 function HomePage() {
   return (
     <div className={css.homePage}>
-      <h1 className={css.homeTitle}>Welcome to our app!</h1>
+      <h1 className={css.homeTitle}>Welcome to our contacts book!</h1>
       <p className={css.homeText}>
-        Now managing your contacts and planning tasks is easier and more
-        convenient. Save and organize your phone book, create tasks, and manage
-        your time with the built-in planner. Everything you need in one place,
-        always at your fingertips!
+        Now managing your contacts is easier and more convenient. Save and
+        organize your phone book. Everything your contacts in one place, always
+        at your fingertips!
       </p>
     </div>
   );
